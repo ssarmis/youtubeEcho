454b70d37c53056de8d15ecb5fb6b79d40b725ae
Silyon <claudiu.dragan52@gmail.com>
2018-05-06 20:30:44 +0300

    Implemented login and registration systems

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const yas = require('youtube-audio-stream');
 const ytpl = require('ytpl');
+const http = require('http');
+const bcrypt = require('bcryptjs');
+const validator = require('express-validator');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 
 const ROOT_PATH = `${__dirname}/layouts/`;
 
@@ -14,6 +20,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/', express.static(ROOT_PATH));
 app.use('/res', express.static(`${ROOT_PATH}res/`));
+app.use(validator());
+
+require('./pass')(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/', (req, res) => {
   res.sendFile(`${ROOT_PATH}/index.html`);
@@ -34,14 +47,66 @@ app.get('/login', (req, res) => {
   res.sendFile(`${ROOT_PATH}/login.html`);
 });
 
-app.post('/login', (req, res) =>{
-  res.sendFile(`${ROOT_PATH}/login.html`);
-  console.log(req.body.username);
-  console.log(req.body.password);
+app.post('/login', (req, res, next) =>{
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/',
+		failureFlash: true
+	})(req, res, next);
 });
 
 app.get('/register', (req, res) =>{
   res.sendFile(`${ROOT_PATH}/register.html`);
+});
+
+app.post('/register', (req, res) => {
+  res.sendFile(`${ROOT_PATH}/register.html`);
+  let user = req.body.username;
+  let pass = req.body.password1;
+  let email = req.body.email;
+  let fname = "First";
+  let lname = "Last";
+  let token = "123";
+  
+  
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid.').isEmail();
+	req.checkBody('password1', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password1);
+	
+	let errors = req.validationErrors();
+	
+	if(!errors){
+			bcrypt.genSalt(5, (err, salt) => {
+				bcrypt.hash(pass, salt, (err, hash) => {
+					if(err){
+						console.log(err);
+						return;
+					}else{
+						pass = hash;
+						let inputData = {username: user, password: pass, first_name: fname, last_name: lname, email:email, token: token};
+						inputData = JSON.stringify(inputData);
+
+						var request = new http.ClientRequest({
+						host: "127.0.0.1",
+						port: 8080,
+						path: "/test/addUser",
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							"Content-Length": Buffer.byteLength(inputData)
+						}
+					});
+					request.end(inputData);
+					
+					request.on("response", response => {
+					  console.log("STATUS: " + response.statusCode);
+					});
+				}
+			});
+		});
+	}
 });
 
 app.get('/reqsgl', (req, res) => {
